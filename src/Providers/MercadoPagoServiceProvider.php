4ad9cdb92dd5b6350ccc1042d1c8e8e3905669d5
William Daniel Portillo <wportillo@teatrix.com>
2018-03-15 10:34:58 -0300

    Update MercadoPagoServiceProvider.php
    Mercado Pago Access Token

--- src/Providers/MercadoPagoServiceProvider.php
+++ src/Providers/MercadoPagoServiceProvider.php
@@ -5,23 +5,20 @@ use LivePixel\MercadoPago\MP;
 
 class MercadoPagoServiceProvider extends ServiceProvider 
 {
-
-	protected $mp_app_id;
-	protected $mp_app_secret;
+	protected $mp_app_access_token;
 
 	public function boot()
 	{
 		
 		$this->publishes([__DIR__.'/../resources/config/mercadopago.php' => config_path('mercadopago.php')]);
 
-		$this->mp_app_id     = config('mercadopago.app_id');
-		$this->mp_app_secret = config('mercadopago.app_secret');
+		$this->mp_app_access_token= config('mercadopago.access_token');
 	}
 
 	public function register()
 	{
 		$this->app->singleton('MP', function(){
-			return new MP($this->mp_app_id, $this->mp_app_secret);
+			return new MP($this->mp_app_access_token);
 		});
 	}
 }
